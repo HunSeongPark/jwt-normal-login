40013bab0d31ea395da859cce9e4114acb6235e3
Hunseong <phunseong@gmail.com>
2022-07-04 13:06:02 +0900

    add: SecurityConfig

---
+++ src/main/java/com/hunseong/jwt/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.hunseong.jwt.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author : Hunseong-Park
+ * @date : 2022-07-04
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests().anyRequest().permitAll();
+    }
+}
