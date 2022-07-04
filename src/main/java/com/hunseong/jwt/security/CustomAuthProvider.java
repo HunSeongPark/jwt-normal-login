841ef1c4739c46840d20bd31314e61ad74e6f245
Hunseong <phunseong@gmail.com>
2022-07-04 13:36:13 +0900

    add: CustomAuthProvider - ID/PW 검증 처리

---
+++ src/main/java/com/hunseong/jwt/security/CustomAuthProvider.java
@@ -0,0 +1,44 @@
+package com.hunseong.jwt.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author : Hunseong-Park
+ * @date : 2022-07-04
+ */
+@RequiredArgsConstructor
+@Component
+public class CustomAuthProvider implements AuthenticationProvider {
+
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = (String) authentication.getCredentials();
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+        // PW 검사
+        if (!passwordEncoder.matches(password, userDetails.getPassword())) {
+            throw new BadCredentialsException("Provider - authenticate() : 비밀번호가 일치하지 않습니다.");
+        }
+
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return true;
+    }
+}
